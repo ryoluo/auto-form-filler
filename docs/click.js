2d37da4e139cb598a8eb13c85cc4ebca0956b125
Ryo@RyoPC <cuba6vst@gmail.com>
2020-03-12 22:50:16 +0900

    update2

--- docs/click.js
+++ docs/click.js
@@ -1,11 +1,11 @@
 function send() {
-  const form = document.getElementById("signup");
+  const form = document.getElementById("register");
   if (form.agree.checked) {
     const title = "[Test] フォームが送信されました。";
     const name = `Name: ${form.name.value}`;
     const email = `Email: ${form.email.value}`;
     const phone = `Phone: ${form.phone.value}`;
-    const location = `Location: ${form.location.value}`;
+    const location = `Location: ${form.area.value}`;
     const msgs = [title, name, email, phone, location];
     res = msgs.join("\n");
     alert(res);
